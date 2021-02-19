aeebbb3856010350ded9767159e09cfb8ba99603
Мария <martynovalala@gmail.com>
2021-02-20 01:21:46 +0200

    masha's func created

--- test_list.py
+++ test_list.py
@@ -13,7 +13,10 @@ class TestListMethods(unittest.TestCase):
 
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
-
+    
+    def test_maria_martynova_fi_93(self)
+        self.assertEqual(len([0]),0)
+        self.assertEqual(len([0]),1)
 
 if __name__ == '__main__':
     unittest.main()
