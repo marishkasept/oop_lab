7313a51a1fb8aff838445fc38149997cadf2d60c
Мария <martynovalala@gmail.com>
2021-02-20 02:35:25 +0200

    broken #43

--- test_list.py
+++ test_list.py
@@ -14,12 +14,11 @@ class TestListMethods(unittest.TestCase):
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
     
-    def test_maria_martynova_fi_93(self)
-        self.assertEqual(len([0]),0)
-        self.assertEqual(len([0]),1)
+    def test_maria_martynova_fi_93(self):
+        self.assertEqual(len([0]), 1)
      
-    def test_maria_martynova_fi_93_2(self)
-        self.assertEqual(len([1, 1]),1)
+    def test_maria_martynova_fi_93_2(self):
+        self.assertEqual(len([1, 1]),2)
 
 if __name__ == '__main__':
     unittest.main()
